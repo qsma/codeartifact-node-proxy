2c0acd1bdb2cbfa40a5ebabc529ffb76607449b4
Bartłomiej Kuśmierczuk <qsma23@gmail.com>
2022-03-06 20:31:40 +0100

    feat: add minimal validation of env variables

--- src/config.ts
+++ src/config.ts
@@ -13,6 +13,17 @@ const { REPO, DOMAIN, OWNER_ID, HOST, ACCESS_TOKENS } = process.env as Record<
   string
 >;
 
+if (
+  ![REPO, DOMAIN, OWNER_ID, HOST, ACCESS_TOKENS].every(
+    (element) => element !== undefined
+  )
+) {
+  console.log(
+    'Error, env configuration is missing for one of the values, use .env.template as template'
+  );
+  process.exit(1);
+}
+
 export const config: Config = {
   repo: REPO,
   domain: DOMAIN,
